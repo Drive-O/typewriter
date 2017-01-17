c4a558558ecd9d98024bc1d2e689f22ea4c48f11
Philipp <PhilSantay@gmail.com>
2017-01-17 21:32:50 +0100

    Gulp installed and PostCSS settings made

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require("gulp"),
+watch = require("gulp-watch"),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+imports = require("postcss-import");
+
+gulp.task("default", function(){
+	console.log("Hooray - you created a Gulp task");
+});
+
+gulp.task("styles", function(){
+	return gulp.src("./assets/styles/style.css")
+	.pipe(postcss([imports, autoprefixer])) //filter, setzt auto Prefixes css
+	.pipe(gulp.dest("./temp/styles")); 
+	//Async func
+})
+
+gulp.task("watch", function(){
+	watch("./assets/styles/**/*.css", function(){
+		gulp.start("styles");
+	});
+})
