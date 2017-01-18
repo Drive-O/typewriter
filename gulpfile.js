7d5dfa512fa62a588cf91874c451292e37d145a2
Philipp <PhilSantay@gmail.com>
2017-01-18 13:43:35 +0100

    Added nesting

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require("gulp"),
 watch = require("gulp-watch"),
 postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
-imports = require("postcss-import");
+imports = require("postcss-import"),
+nesting = require("postcss-nesting");
 
 gulp.task("default", function(){
 	console.log("Hooray - you created a Gulp task");
@@ -10,7 +11,7 @@ gulp.task("default", function(){
 
 gulp.task("styles", function(){
 	return gulp.src("./assets/styles/style.css")
-	.pipe(postcss([imports, autoprefixer])) //filter, setzt auto Prefixes css
+	.pipe(postcss([imports, autoprefixer, nesting])) //filter, setzt auto Prefixes css
 	.pipe(gulp.dest("./temp/styles")); 
 	//Async func
 })
